97dab9236ebc84f4e847daa7493c1c82f568c4a1
Umair Tahir <umair@metalogixtech.com>
2024-07-02 15:11:19 +0500

    modal issue

--- application/src/pages/AnOtherPage.jsx
+++ application/src/pages/AnOtherPage.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState } from "react";
 import ReactEditorKit from "react-text-editor-kit";
+// import ReactEditorKit from "react-text-editor-kit";
 
 export default function AnOtherPage() {
   const [value, setValue] = useState("");
-  const [value1, setValue1] = useState("");
-  const [value2, setValue2] = useState("");
-  const { state } = useLocation();
-
-  // useEffect(() => {
-  //   if (state) {
-  //     setValue(String(state.detailed_description));
-  //   }
-  // }, []);
 
   return (
     <>
@@ -21,18 +12,6 @@ export default function AnOtherPage() {
         onChange={setValue}
         mainProps={{ className: "red" }}
         placeholder="Write your text here 1"
-      />
-      <ReactEditorKit
-        value={value1}
-        onChange={setValue1}
-        mainProps={{ className: "red" }}
-        placeholder="Write your text here 2"
-      />
-      <ReactEditorKit
-        value={value2}
-        onChange={setValue2}
-        mainProps={{ className: "red" }}
-        placeholder="Write your text here 3"
       />
     </>
   );
