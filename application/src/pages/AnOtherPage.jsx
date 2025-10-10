7e6a3a7e48e5de590b16cb73612adf24e0b97259
Haseeb Islam <haseeb.islam22@gmail.com>
2025-10-10 17:11:11 +0500

    del node module

--- application/src/pages/AnOtherPage.jsx
+++ application/src/pages/AnOtherPage.jsx
@@ -12,7 +12,7 @@ export default function AnOtherPage() {
         onChange={setValue}
         mainProps={{ className: "red" }}
         placeholder="Please Write Something..."
-        apiKey={"a3eb9a4f-6758-4c3e-ba0f-75ddf059f3c7"}
+        apiKey={""}
         height={"400px"}
       />
       {/* <ReactEditorKit
